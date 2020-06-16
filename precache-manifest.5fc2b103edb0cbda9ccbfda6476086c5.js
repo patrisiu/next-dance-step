d39d6afe195435a4bf647e356ef8a99decb27f96
patrisiu <patrisiu@debian.casanova>
2020-06-16 11:59:42 +0200

    Updates

---
+++ precache-manifest.5fc2b103edb0cbda9ccbfda6476086c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fd9cf39ea3272ee8472925f7a17dc5",
+    "url": "/next-dance-step/index.html"
+  },
+  {
+    "revision": "92bf98c77adfa5c31d44",
+    "url": "/next-dance-step/static/css/main.4bf3c838.chunk.css"
+  },
+  {
+    "revision": "bf50647dd26a018a82af",
+    "url": "/next-dance-step/static/js/2.806793cd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/next-dance-step/static/js/2.806793cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92bf98c77adfa5c31d44",
+    "url": "/next-dance-step/static/js/main.96232c0c.chunk.js"
+  },
+  {
+    "revision": "adf7b2e808cdcf699d90",
+    "url": "/next-dance-step/static/js/runtime-main.707c14b2.js"
+  },
+  {
+    "revision": "405bbf4ee94d593cb4febe80aea3d8da",
+    "url": "/next-dance-step/static/media/wcs.405bbf4e.png"
+  }
+]);
